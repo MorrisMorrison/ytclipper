cd22b19511357c45907a3bcdb24dcfe6f083571a
Morris Morrison <mwlltrdev@gmail.com>
2021-07-09 17:02:06 +0200

    - create basic structure - setup tailwind

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+const port = 4001;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.post("/api/v1/getclip", (req, res) =>{
+  videoPath = downloadVideo(req.params["url"]);  
+  clipPath = cutVideo = (videoPath, req.params["from"], req.params["to"])
+  res.status = 200;
+  if (clipPath === '') res.status = 400;
+  res.download(clipPath)  
+})
+
+app.listen(port, () => {
+  console.log(`> --- START YTCLIPPER ---`);
+  console.log(`> --- LISTENING AT PORT ${port} ---`);
+})
+
+module.exports = app;
---
+++ videoprocessing.js
@@ -0,0 +1,14 @@
+const processVideo =  (url) => {
+
+}
+
+const downloadVideo = (url) => {
+    // download video with ytcore or youtubedl
+    return '';
+}
+
+
+const cutVideo = (path, from, to )=>{
+    // cut video with ffmpeg
+    return '';
+}
