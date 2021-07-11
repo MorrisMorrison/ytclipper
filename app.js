508cfb70f89f80809d8162c2afbbfec283266b19
Morris Morrison <mwlltrdev@gmail.com>
2021-07-11 16:51:16 +0200

    use port provided by heroku or default to 4001

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api')
 
 var app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
