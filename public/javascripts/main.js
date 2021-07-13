608302be5304b2e9aad054ed078924e8a0965b75
Morris Morrison <mwlltrdev@gmail.com>
2021-07-13 02:52:04 +0200

    - validate input on client and server - use toastr to show messages

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,33 +1,87 @@
+const timeObject2Seconds = (time) =>  (time.hours / 60 / 60) + (time.minutes /60) + time.seconds;
+const isTimeInputValid = (time) => /([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/g.test(time);
+// const isTimeInputValid = (time) => time.match(/^\d?\d(?::\d{2}){2}$/);
+const isYoutubeUrlValid = (url) => new RegExp("/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/").test(url);
+const getTimeAsObject = (time) => {
+
+    const arr = time.split(':');
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+
+    switch (arr.length) {
+        case 0:
+            seconds = arr[0];
+            break;
+        case 2:
+            minutes = arr[0];
+            seconds = arr[1];
+            break;
+        case 3:
+            hours = arr[0];
+            minutes = arr[1];
+            seconds = arr[2];
+            break;
+    }
+
+    return {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    }
+}
+
+
 const onClipButtonClick = () => {
     const url = 'http://localhost:4001/api/v1/createclip'
+
+    const youtubeUrl = document.getElementById('url').value;
+    let from = document.getElementById('from').value;
+    let to = document.getElementById('to').value;
+
+    if (url == '' || from == '' || to == '') {
+        toastr.error('Please provide a url and both timestamps.');
+        return;
+    }
+
+    if (!isTimeInputValid(from) || !isTimeInputValid(to)) {
+        toastr.error('Please provide timestamps as HH:MM:SS.');
+        return;
+    }
+
+
+    from = timeObject2Seconds(getTimeAsObject(from));
+    to = timeObject2Seconds(getTimeAsObject(to));
+    const duration = to - from;
+
+    console.log(from);
+    console.log(to);
+    console.log(duration);
+
     const payload = JSON.stringify({
-        url: document.getElementById('url').value,
-        from: document.getElementById('from').value,
-        to: document.getElementById('to').value,
+        url: youtubeUrl,
+        from: from,
+        to: duration,
     });
 
     const headers = {
         'content-type': 'application/json'
     };
 
-    console.log(payload);
-
     fetch(url, {
         method: 'POST',
         headers: headers,
         body: payload,
     }).then(
-        // wartedialog anzeigen
-        // status abfragen
         response => {
+            toastr.success('Started creating clip.');
             if (response.status == 200) {
                 console.log('CLIENT -- SUCCESS!')
                 const videoName = response.text();
                 console.log('CLIENT -- VIDEO NAME: ' + videoName);
                 videoName.then(resolve => {
                     console.log('RESOLVED VIDEO NAME: ' + resolve)
-                    window.open('/api/v1/download?videoName='+ resolve);
-                    // downloadVideo(resolve);
+                    window.open('/api/v1/download?videoName=' + resolve);
                 })
             }
         }
@@ -36,22 +90,20 @@ const onClipButtonClick = () => {
     );
 }
 
-const showWaitingDialog = () => {
-
-}
-const removeWaitingDialog = () => {
-
-}
-
-
 const downloadVideo = (videoName) => {
     const url = 'http://localhost:4001/api/v1/download?videoName=' + videoName;
     fetch(url, {
         method: 'GET',
     }).then(res => {
-        if (res.status == 200){
+        if (res.status == 200) {
             console.log('CLIENT -- DOWNLOAD VIDEO');
             console.log('CLIENT -- DOWNLOAD VIDEO URL: ' + url);
         }
     });
+}
+
+const resetInputValues = () => {
+    document.getElementById('url').value = '';
+    document.getElementById('from').value = '';
+    document.getElementById('to').value = '';
 }
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 var videoprocessing = require('../videoprocessing')
 
-
 // https://www.youtube.com/watch?v=RKYFcHv3DPg
-var filename = `/home/morrismorrison/Development/ytclipper/videos/myvideo.mp4`;
-var clipname = `/home/morrismorrison/Development/ytclipper/videos/myclip.mp4`;
+
+const filename = `/home/morrismorrison/Development/ytclipper/videos/myvideo.mp4`;
+const clipname = `/home/morrismorrison/Development/ytclipper/videos/myclip.mp4`;
+const isYoutubeUrlValid = (url) => /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/.test(url);
+const isCreateClipRequestValid = (req) => req.body == '' || req.body.url == '' || !isYoutubeUrlValid(req.body.url);
 
 router.post('/createclip', async (req, res) =>{
+  if(!isCreateClipRequestValid(req)) {
+    res.status = 500;
+    return;
+  }
   console.log("SERVER -- CLIENT REQUEST -- CREATE CLIP");
   // var jobId = videoprocessing.downloadVideo(req.body.url, () => videoprocessing.cutVideo(filename, req.body.from, req.body.to));
   var stuff = videoprocessing.downloadVideoAsync(req.body.url, () => videoprocessing.cutVideoAsync(filename, req.body.from, req.body.to))
@@ -18,11 +24,13 @@ router.post('/createclip', async (req, res) =>{
                       });                      
 })
 
-router.get('status', (req, res) => {
-  console.log("SERVER -- CLIENT REQUEST -- STATUS");
-})
-
+const isDownloadRequestValid = (req) => req.query.videoName != '';
 router.get('/download', (req, res) => {
+  if(!isDownloadRequestValid(req)) {
+    res.status = 500;
+    return;
+  }
+
   console.log('SERVER -- CLIENT REQUEST -- DOWNLOAD');
   console.log('SERVER -- VIDEO NAME: ' + req.query.videoName);
   res.download(clipname)
