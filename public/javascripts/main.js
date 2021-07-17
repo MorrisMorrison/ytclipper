820ad4ca358391e6dcf15ed005f3f55f5389de5c
Morris Morrison <mwlltrdev@gmail.com>
2021-07-17 15:21:42 +0200

    - process videos as child processes and save status in a queue - the client checks for the status of the job every 2 seconds instead of waiting for the videoprocessing to finish on serverside, which caused performance issues

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -65,7 +65,7 @@ const onClipButtonClick = () => {
         return;
     }
 
-    if(!isYoutubeUrlValid(youtubeUrl)){
+    if (!isYoutubeUrlValid(youtubeUrl)) {
         toastr.error('Please provide a valid youtube url.', 'Invalid Url')
         return;
     }
@@ -78,10 +78,8 @@ const onClipButtonClick = () => {
             toastr.error('Please use timestamps that are within the videos duration.', 'Invalid Timestamps.');
             return;
         }
-        toastr.success('The download will pop up automatically. This may take a few seconds.', "Download Started.");
 
         const duration = to - from;
-
         const payload = JSON.stringify({
             url: youtubeUrl,
             from: from,
@@ -99,14 +97,17 @@ const onClipButtonClick = () => {
         }).then(
             response => {
                 switch (response.status) {
-                    case 200:
-                        const videoName = response.text();
-                        videoName.then(resolve => {
-                            window.open('/api/v1/download?videoName=' + resolve);
+                    case 201:
+                        toastr.success('The download will pop up automatically. This may take a few seconds.', "Download Started.");
+                        const jobId = response.text();
+                        jobId.then(jId => {
+                            getJobStatus(jId);
                         });
                         break;
                     case 500:
                         toastr.error('Timestamps are not within video length.');
+                        break;
+
                 }
             }
         ).then(
@@ -125,6 +126,32 @@ const downloadVideo = (videoName) => {
     });
 }
 
+const getJobStatus = (jobId) => {
+    const url = window.location.href + 'api/v1/getjobstatus?jobId=' + jobId;
+    fetch(url, {
+        method: 'GET',
+    }).then(res => {
+        switch (res.status) {
+            case 200:
+                res.text().then(result => {
+                    window.open('/api/v1/download?videoName=' + result);
+                })
+                break;
+            case 201:
+                setTimeout(() => getJobStatus(jobId), 2000);
+                break;
+            case 400:
+                toastr.error('An error occurred when getting the job status. Please try again in a few minutes or use the contact form.', 'Get Job Status')
+                break;
+            case 408:
+                toastr.error('A timeout occurred when getting the job status. Please try again in a few minutes or use the contact form.', 'Get Job Status')
+                break;
+        }
+
+    });
+
+
+}
 const resetInputValues = () => {
     document.getElementById('url').value = '';
     document.getElementById('from').value = '';
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const videoprocessing = require('../videoprocessing');
 const path = require('path');
-const {fork} = require('child_process');
+const { fork } = require('child_process');
+const { strict } = require('assert');
+const { json } = require('body-parser');
 
 const appDir = path.dirname(require.main.filename);
 
@@ -21,52 +23,113 @@ const getVideoIdByYoutubeUrl = (url) => {
     return (match && match[7].length == 11) ? match[7] : false;
 }
 
-router.post('/createclip', (req, res) => {
-    // videoprocessing.getVideoDurationAsync(req.body.url).then(duration => {
-    //   if (!areTimestampsWithinVideoDuration(req.from, req.to, duration)){
-    //     res.status = 500;
-    //     return;
-    //   }
+const jobStatus = {
+    RUNNING: "running",
+    DONE: "done",
+    ERROR: "error",
+    TIMEOUT: "timeout",
+    CREATED: "created"
+}
+var jobQueue = [];
 
+const getJobId = () => {
+    for (i = 0; i < 10; i++) {
+        if (jobQueue[i] === undefined || jobQueue[i] === '' && jobQueue[i] || {}) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+router.get('/getjobstatus', (req, res) => {
+    if (jobQueue[req.query.jobId] != undefined && jobQueue[req.query.jobId] != '' && jobQueue[req.query.jobId] != {}) {
+        switch (jobQueue[req.query.jobId].status) {
+            case 'created':
+                res.status(201).send();
+                break;
+            case 'running':
+                res.status(201).send();
+                break;
+            case 'done':
+                res.status(200).send(jobQueue[req.query.jobId].clipName);
+                break;
+            case 'timeout':
+                res.status(408).send();
+                break;
+            case 'error':
+                res.status(500).send();
+                break;
+        }
+    }
+
+    res.status(400);
+})
+
+router.post('/createclip', (req, res) => {
+    console.log('SERVER - CREATECLIP')
     if (!isCreateClipRequestValid(req)) {
         res.status = 500;
         return;
     }
-    console.log('CREATECLIP HAAALO')
+    console.log('SERVER - CREATECLIP - Request is valid.')
+
+
+    var jobId = getJobId();
+    // Everything is busy
+    if (jobId == -1) {
+        res.status = 500;
+        return;
+    }
+
+    console.log('SERVER - CREATECLIP - JobId is available.')
+
+    jobQueue[jobId] = {
+        status: jobStatus.CREATED,
+    }
+
+    console.log('SERVER - CREATECLIP - Job created with id ' + jobId + '.')
+
     const videoId = getVideoIdByYoutubeUrl(req.body.url);
     const fileName = videoPathTemplate + videoId + videoFileNameEnding;
     const clipName = videoPathTemplate + videoId + clipFileNameEnding;
-    console.log('CREATECLIP HAAALO22222')
+
+    console.log('SERVER - CREATECLIP - VideoId extracted.')
 
     const processDownloadVideo = fork('./downloadVideo.js');
     const processCutVideo = fork('./cutVideo.js');
+    const clipFileName = videoId + '_clip.mp4';
 
-    console.log('CREATECLIP HAAALO2222233333')
-    const clipFileName =  videoId + '_clip.mp4';
+    processDownloadVideo.send({
+        url: req.body.url,
+        fileName: fileName,
+        videoName: videoId + '.mp4'
+    });
 
+    console.log('SERVER - CREATECLIP - Downloading video in child process started.')
 
-    processDownloadVideo.send({url: req.body.url,
-    fileName: fileName,
-    videoName: videoId + '.mp4'});
     processDownloadVideo.on('message', async (processDownloadResult) => {
-        console.log(processDownloadResult);
-        console.log('DOWNLOAD VIDEO FINISHED IN CHILD PROCESS !!!!')
+        console.log('SERVER - CREATECLIP - Downloading video finished.')
         processCutVideo.send({
             fileName: fileName,
             clipName: clipName,
             from: req.body.from,
             to: req.body.to
         })
+        console.log('SERVER - CREATECLIP - Cutting video started.')
+
 
         processCutVideo.on('message', (processCutResult) => {
-            console.log('CUT VIDEO FINISHED IN CHILD PROCESS !!!!')
-            console.log('CLIPNAME!!!' + clipFileName);
-            res.send(clipFileName);
+            console.log('SERVER - CREATECLIP - Cutting video finished.')
+            jobQueue[jobId] = {
+                status: jobStatus.DONE,
+                clipName: clipFileName
+            };
         })
     })
-    // await videoprocessing.downloadVideoAsync(req.body.url, fileName, videoId + ".mp4");
-    // await videoprocessing.cutVideoAsync(fileName, clipName, req.body.from, req.body.to);
-    // })
+
+    res.status(201).send(JSON.stringify(jobId));
 })
 
 const isDownloadRequestValid = (req) => req.query.videoName != '';
