0a543f48856fcf02518ae060f35059eeecadb6bc
Robert Mang <Robert.Mang@softgarden.de>
2023-11-18 02:12:23 +0100

    improve error handling and logging

--- core/cutVideo.js
+++ core/cutVideo.js
@@ -1,7 +1,14 @@
 const videoprocessing = require('./videoprocessing');
 
-process.on('message', async(message) => {
-    console.log("SERVER - CUTVIDEOASYNC - Cut video " + message.fileName)
-    const result = await videoprocessing.cutVideoAsync(message.fileName, message.clipName, message.from, message.to);
-    process.send(result);
-})
+process.on('message', async (message) => {
+    console.log("CUTVIDEOASYNC - Cut video " + message.fileName);
+    
+    try {
+        const result = await videoprocessing.cutVideoAsync(message.fileName, message.clipName, message.from, message.to);
+        process.send(result);
+    } catch (error) {
+        console.error("CUTVIDEOASYNC - Error processing video: ", error.message);
+        process.send({ error: error.message });
+    }
+});
+
--- core/downloadVideo.js
+++ core/downloadVideo.js
@@ -1,6 +1,13 @@
 const videoprocessing = require('./videoprocessing');
 
-process.on('message', async(message) => {
-    const result = await videoprocessing.downloadVideoAsync(message.url, message.fileName, message.videoName);
-    process.send(result);
-})
+process.on('message', async (message) => {
+    try {
+        console.log(`Downloading video from ${message.url} to ${message.fileName}`);
+        const result = await videoprocessing.downloadVideoAsync(message.url, message.fileName, message.videoName);
+        console.log('Video download completed successfully.');
+        process.send(result);
+    } catch (error) {
+        console.error('Error during video download:', error);
+        process.send({ error: error.message });
+    }
+});
