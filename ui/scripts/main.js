c17b0114f46a10ca6802e04f3fe8a132e58b27b9
Robert Mang <Robert.Mang@softgarden.de>
2023-11-18 01:43:56 +0100

    refactoring

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -1,51 +1,51 @@
-const timeObject2Seconds = (time) => (time.hours * 60 * 60) + (time.minutes * 60) + time.seconds;
+const timeObjectToSeconds = (time) => (time.hours * 60 * 60) + (time.minutes * 60) + time.seconds;
+
 const isTimeInputValid = (time) => /([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/g.test(time);
+
 const isYoutubeUrlValid = (url) => /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/.test(url);
+
 const isTimestampWithinDuration = (timestamp, duration) => timestamp <= duration;
 
 const getTimeAsObject = (time) => {
     const [hours = 0, minutes = 0, seconds = 0] = time.split(':').map(Number);
-    return {
-        hours,
-        minutes,
-        seconds
-    };
+    return { hours, minutes, seconds };
 };
 
 const showProgressBar = () => document.getElementById("progressBarWrapper").classList.remove('hidden');
+
 const hideProgressBar = () => document.getElementById("progressBarWrapper").classList.add('hidden');
 
 const showDownloadLink = (downloadUrl) => {
     const downloadLinkUrlWrapper = document.getElementById("downloadLinkWrapper");
-    
     const downloadLink = document.getElementById("downloadLink");
     downloadLink.setAttribute('href', downloadUrl);
-
     downloadLinkUrlWrapper.classList.remove('hidden');
-}
+};
 
-const getVideoDuration = async (youtubeUrl) =>
-    new Promise(resolve => {
-        const url = window.location.href + "api/v1/getvideoduration?youtubeUrl=" + youtubeUrl;
-        fetch(url, {
-            method: 'GET',
-        }).then(res => {
-            if (res.status = 200) {
-                const durationInSeconds = res.text();
-                resolve(durationInSeconds);
-            }
-        });
-    });
+const getVideoDuration = async (youtubeUrl) => {
+    const url = window.location.href + "api/v1/getvideoduration?youtubeUrl=" + youtubeUrl;
+    try {
+        const res = await fetch(url, { method: 'GET' });
+        if (res.status === 200) {
+            const durationInSeconds = await res.text();
+            return durationInSeconds;
+        }
+    } catch (error) {
+        console.error("Error fetching video duration:", error);
+        // Handle error
+    }
+};
 
-const onClipButtonClick = () => {
-    const url = window.location.href + 'api/v1/createclip'
+const convertToSeconds = (timeString) => timeObjectToSeconds(getTimeAsObject(timeString));
 
+const onClipButtonClick = async () => {
+    const url = window.location.href + 'api/v1/createclip';
     const youtubeUrl = document.getElementById('url').value;
     let from = document.getElementById('from').value;
     let to = document.getElementById('to').value;
 
-    if (url == '' || from == '' || to == '') {
-        toastr.error('Please provide a url and both timestamps.', 'Invalid Inout');
+    if (url === '' || from === '' || to === '') {
+        toastr.error('Please provide a URL and both timestamps.', 'Invalid Input');
         return;
     }
 
@@ -55,107 +55,94 @@ const onClipButtonClick = () => {
     }
 
     if (!isYoutubeUrlValid(youtubeUrl)) {
-        toastr.error('Please provide a valid youtube url.', 'Invalid Url')
+        toastr.error('Please provide a valid YouTube URL.', 'Invalid Url');
         return;
     }
 
-    getVideoDuration(youtubeUrl).then(videoDuration => {
-        from = timeObject2Seconds(getTimeAsObject(from));
-        to = timeObject2Seconds(getTimeAsObject(to));
-        videoDuration = timeObject2Seconds(getTimeAsObject(videoDuration));
+    try {
+        const videoDuration = await getVideoDuration(youtubeUrl);
+        from = convertToSeconds(from);
+        to = convertToSeconds(to);
+        const duration = convertToSeconds(videoDuration);
 
-        if (!isTimestampWithinDuration(parseInt(from), videoDuration) || !isTimestampWithinDuration(parseInt(to), videoDuration)) {
-            toastr.error('Please use timestamps that are within the videos duration.', 'Invalid Timestamps.');
+        if (!isTimestampWithinDuration(from, duration) || !isTimestampWithinDuration(to, duration)) {
+            toastr.error('Please use timestamps that are within the video\'s duration.', 'Invalid Timestamps');
             return;
         }
 
-        const duration = to - from;
         const payload = JSON.stringify({
             url: youtubeUrl,
             from: from,
-            to: duration,
+            to: to - from,
         });
 
         const headers = {
-            'content-type': 'application/json'
+            'content-type': 'application/json',
         };
 
-        fetch(url, {
+        const response = await fetch(url, {
             method: 'POST',
             headers: headers,
             body: payload,
-        }).then(
-            response => {
-                switch (response.status) {
-                    case 201:
-                        toastr.success('The download will pop up automatically. This may take a few seconds.', "Download Started.");
-                        showProgressBar();
+        });
 
-                        const jobId = response.text();
-                        jobId.then(jId => {
-                            getJobStatus(jId);
-                        });
-                        break;
-                    case 500:
-                        toastr.error('Timestamps are not within video length.');
-                        break;
+        switch (response.status) {
+            case 201:
+                toastr.success('The download will pop up automatically. This may take a few seconds.', 'Download Started');
+                showProgressBar();
+                const jobId = await response.text();
+                getJobStatus(jobId);
+                break;
+            case 500:
+                toastr.error('Timestamps are not within video length.');
+                break;
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+};
 
-                }
-            }
-        ).then(
-            html => console.log(html)
-        );
-    });
-}
-
-const getJobStatus = (jobId) => {
+const getJobStatus = async (jobId) => {
     const url = window.location.href + 'api/v1/getjobstatus?jobId=' + jobId;
-    fetch(url, {
-        method: 'GET',
-    }).then(res => {
+
+    try {
+        const res = await fetch(url, { method: 'GET' });
+
         switch (res.status) {
             case 200:
-                res.text().then(result => {
-                    hideProgressBar();
-                    const downloadUrl = '/api/v1/download?videoName=' + result; 
-                    showDownloadLink(downloadUrl);
-                    window.open(downloadUrl);
-                })
+                const result = await res.text();
+                hideProgressBar();
+                const downloadUrl = '/api/v1/download?videoName=' + result;
+                showDownloadLink(downloadUrl);
+                window.open(downloadUrl);
                 break;
             case 201:
                 setTimeout(() => getJobStatus(jobId), 2000);
                 break;
             case 400:
-                toastr.error('An error occurred when getting the job status. Please try again in a few minutes or use the contact form.', 'Get Job Status')
-                break;
             case 408:
-                toastr.error('A timeout occurred when getting the job status. Please try again in a few minutes or use the contact form.', 'Get Job Status')
+                toastr.error('An error occurred when getting the job status. Please try again in a few minutes or use the contact form.', 'Get Job Status');
                 break;
         }
-
-    });
-}
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+};
 
 const handleDarkMode = () => {
-    console.log(localStorage.theme);
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark')
+        document.documentElement.classList.add('dark');
     } else {
-        document.documentElement.classList.remove('dark')
+        document.documentElement.classList.remove('dark');
     }
-}
+};
 
-const setTheme = ()=> {
-    if (localStorage.theme === 'dark'){
-        localStorage.theme = 'light'
-    }
-    else if(localStorage.theme === 'light'){
-        localStorage.theme = 'dark'
-    }
+const setTheme = () => {
+    localStorage.theme = localStorage.theme === 'dark' ? 'light' : 'dark';
     handleDarkMode();
-}
+};
 
 window.onload = () => {
     localStorage.theme = 'dark';
     handleDarkMode();
-}
+};
